0009afe99127d56e03ac398bddadbf1b34707dba
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-09-04 00:51:56 -0600

    Update

--- auto_seismo/src/write_pred.py
+++ auto_seismo/src/write_pred.py
@@ -18,9 +18,9 @@ def write_pred(datadir, files, preds):
     #files = np.sort(os.listdir(npzdir))
     for i, file in enumerate(files):
         seismogram = obspy.read(datadir + file + '.s_fil')
-        maxima_times = seismogram[0].times()[sp.signal.argrelmax(seismogram[0].data)]
+        #maxima_times = seismogram[0].times()[sp.signal.argrelmax(seismogram[0].data)]
         pred_time = preds[i]
-        pred_time = maxima_times[np.argmin(np.abs(maxima_times - pred_time))]
+        #pred_time = maxima_times[np.argmin(np.abs(maxima_times - pred_time))]
         seismogram[0].stats.sac.t6 = pred_time
         #seismogram[0].stats.sac.e += shift
         seismogram.write(datadir + file + '.s_fil', format='SAC')
