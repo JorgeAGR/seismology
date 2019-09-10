970b78f5480aa0d207fc88126636de3af1520c97
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-09-09 18:48:45 -0600

    Update

--- auto_seismo/src/train_models.py
+++ auto_seismo/src/train_models.py
@@ -10,25 +10,47 @@ import os
 import numpy as np
 from models import pred_Time_Model
 from aux_funcs import read_config, make_dirs
+import tensorflow as tf
+from keras.backend import tensorflow_backend as tfb
 
 make_dirs()
 
+tf_config = read_config('./config/tf_config.txt')
 pos_config = read_config('./config/pos_model_config.txt')
 neg_config = read_config('./config/neg_model_config.txt')
 
 models = (pos_config, neg_config)
 
-for model in models:
-    print('Creating new model:', model['model_name'])
-    if model['model_type'] == 'regression':
-        if model['debug_mode']:
-            model['model_name'] += '_debug'
-        
-        pred_Time_Model(model['model_name'],
-                        model['train_dir'], 
-                        model['seismos_train'],
-                        model['arrivals_train'],
-                        model['batch_size'],
-                        model['epochs'],
-                        model['model_iters'],
-                        debug_mode=model['debug_mode'])
+if not tf_config['gpu']:
+    with tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=tf_config['nodes']*tf_config['threads'])) as sess:
+        tfb.set_session(sess)
+        for model in models:
+            print('Creating new model:', model['model_name'])
+            if model['model_type'] == 'regression':
+                if model['debug_mode']:
+                    model['model_name'] += '_debug'
+                
+                pred_Time_Model(model['model_name'],
+                                model['train_dir'], 
+                                model['seismos_train'],
+                                model['arrivals_train'],
+                                model['batch_size'],
+                                model['epochs'],
+                                model['model_iters'],
+                                debug_mode=model['debug_mode'])
+                
+else:
+    for model in models:
+            print('Creating new model:', model['model_name'])
+            if model['model_type'] == 'regression':
+                if model['debug_mode']:
+                    model['model_name'] += '_debug'
+                
+                pred_Time_Model(model['model_name'],
+                                model['train_dir'], 
+                                model['seismos_train'],
+                                model['arrivals_train'],
+                                model['batch_size'],
+                                model['epochs'],
+                                model['model_iters'],
+                                debug_mode=model['debug_mode'])
