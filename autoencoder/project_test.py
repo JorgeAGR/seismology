f7badea34a775d0d4283af7821f75b7e88af038e
jorgeagr <jorgeagr@discovery-l1.nmsu.edu>
2019-10-22 20:31:39 -0600

    Update

--- autoencoder/project_test.py
+++ autoencoder/project_test.py
@@ -14,7 +14,7 @@ from keras.models import Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.optimizers import Adam
 
-''''
+'''
 # This stumped me! I can't think recursively. Figure it out later.
 def get_Decoder_Output(autoencoder, compression_size, layer, i=1, layers=12):
     if i == layers:
@@ -51,6 +51,52 @@ def get_Decoder_Layers(autoencoder, layers=12):
         
     return decoder_layers
 
+
+def AutoEncoder(input_length, compression_size):
+    input_seis = Input(shape=(input_length, 1))
+    
+    dense1 = Dense(input_length//2, activation='sigmoid')(input_seis)
+    encoded = Dense(input_length//4, activation='sigmoid')(dense1)
+    dense_up1 = Dense(input_length//2, activation='sigmoid')(encoded)
+    decoded = Dense(input_length, activation='sigmoid')(dense_up1)
+
+    autoencoder = Model(input_seis, decoded)
+    autoencoder.compile(loss='mean_squared_error', optimizer=Adam())
+
+    return autoencoder, None, None
+
+def CAE(input_length, compression_size):
+    input_seis = Input(shape=(input_length, 1))
+
+    conv1 = Conv1D(32, kernel_size=3, strides=1,
+                     activation='relu', padding='same')(input_seis)
+    bn1 = BatchNormalization()(conv1)
+    max1 = MaxPooling1D(pool_size=2)(bn1)
+
+    convup1 = Conv1D(32, kernel_size=3, strides=1,
+                     activation='relu', padding='same')(max1)
+    bn_up1 = BatchNormalization()(convup1)
+    up1 = UpSampling1D(size=2)(bn_up1)
+
+    decoding = Conv1D(1, kernel_size=21, strides=1,
+                     activation='sigmoid', padding='same')(up1)
+
+    autoencoder = Model(input_seis, decoding)
+
+    autoencoder.compile(loss='binary_crossentropy',#huber_loss,
+                  optimizer=Adam(1e-2))
+
+    return autoencoder, None, None#decoder
+
+
+
+
+
+
+
+
+
+
 def rossNet_CAE(input_length, compression_size):
     input_seis = Input(shape=(input_length, 1))
 
@@ -69,16 +115,18 @@ def rossNet_CAE(input_length, compression_size):
     bn3 = BatchNormalization()(conv3)
     max3 = MaxPooling1D(pool_size=2)(bn3)
 
-    flattened = Flatten()(max3)
+    #flattened = Flatten()(max3)
     
-    encoding = Dense(compression_size, activation='relu')(flattened)
+    #dense_d1 = Dense(500, activation='sigmoid')
     
-    expand = Dense(max3.shape.as_list()[1] * max3.shape.as_list()[2], activation='relu')(encoding)
+    #encoding = Dense(compression_size, activation='sigmoid')(flattened)
     
-    reshaped = Reshape(max3.shape.as_list()[1:])(expand)
+    #expand = Dense(max3.shape.as_list()[1] * max3.shape.as_list()[2], activation='sigmoid')(encoding)
+    
+    #reshaped = Reshape(max3.shape.as_list()[1:])(expand)
     
     convup1 = Conv1D(128, kernel_size=11, strides=1,
-                     activation='relu', padding='same')(reshaped)
+                     activation='relu', padding='same')(max3)#(reshaped)
     bn_up1 = BatchNormalization()(convup1)
     up1 = UpSampling1D(size=2)(bn_up1)
     
@@ -97,18 +145,19 @@ def rossNet_CAE(input_length, compression_size):
     
     autoencoder = Model(input_seis, decoding)
     
-    decoder_input = Input(shape=(compression_size,))
+    #decoder_input = Input(shape=(compression_size,))
     decoder_layers = get_Decoder_Layers(autoencoder)
     
-    encoder = Model(input_seis, encoding)
-    decoder = Model(decoder_input,
-                    decoder_layers[0](decoder_layers[1](decoder_layers[2](decoder_layers[3](decoder_layers[4](decoder_layers[5](decoder_layers[6](decoder_layers[7](decoder_layers[8](decoder_layers[9](decoder_layers[10](decoder_layers[11](decoder_input)))))))))))))
+    encoder = Model(input_seis, max3)#encoding)
+    decoder_input = Input(shape=encoder.get_output_shape_at(0))
+    #decoder = Model(decoder_input,
+    #                decoder_layers[0](decoder_layers[1](decoder_layers[2](decoder_layers[3](decoder_layers[4](decoder_layers[5](decoder_layers[6](decoder_layers[7](decoder_layers[8](decoder_layers[9](decoder_layers[10](decoder_layers[11](decoder_input)))))))))))))
 
     # for losses either binary crossentropy or MSE
     autoencoder.compile(loss='binary_crossentropy',#huber_loss,
                   optimizer=Adam(1e-2))
 
-    return autoencoder, encoder, decoder
+    return autoencoder, encoder, None#decoder
 
 # Load training and testing data
 batch_size = 128
@@ -118,7 +167,7 @@ x_test = np.load('data/test/test_seismos.npy')
 x_train = (x_train + 1)/2
 x_test = (x_test + 1)/2
 
-autoencoder, encoder, decoder = rossNet_CAE(x_train.shape[1], 320)
+autoencoder, encoder, decoder = AutoEncoder(x_train.shape[1], 320)
 autoencoder.fit(x_train, x_train,
                 validation_data=(x_test, x_test),
                 verbose=2,
@@ -126,6 +175,6 @@ autoencoder.fit(x_train, x_train,
                 epochs=epochs,
                 callbacks=get_Callbacks(epochs))
 
-autoencoder.save('autoencoder.h5')
-encoder.save('encoder.h5')
-decoder.save('decoder.h5')
+#autoencoder.save('autoencoder.h5')
+#encoder.save('encoder.h5')
+#decoder.save('decoder.h5')
