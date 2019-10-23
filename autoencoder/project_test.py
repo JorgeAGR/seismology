1b646a3233bc8e4fa496c485c4cd4f91700a9a6c
jorgeagr <jorgeagr@discovery-l1.nmsu.edu>
2019-10-23 01:11:22 -0600

    Update

--- autoencoder/project_test.py
+++ autoencoder/project_test.py
@@ -205,4 +205,4 @@ def train_Model(model_class, model_name):
     #decoder.save('decoder.h5')
 
 
-train_Model(RossNet_CAE, 'rossnet_convautoencoder_bce')
+train_Model(RossNet_CAE, 'rossnet_convautoencoder_mse')
