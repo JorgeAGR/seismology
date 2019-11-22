a2296769ecf3c3b14e7c9a2a27a90c77b0a03e15
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-11-22 09:02:53 -0700

    Update

--- exp_src/quality_catalog.py
+++ exp_src/quality_catalog.py
@@ -42,6 +42,19 @@ def cut_Window(cross_sec, times, t_i, t_f):
     
     return cross_sec[init:end]
 
+def get_Lauren_Pred(cap, precursor, file):
+    file_path = 'cross_secs_dat/lauren_pred/' + cap + 'caps_deg.dat'
+    if precursor == '410':
+        ind = 1
+    elif precursor == '660':
+        ind = 4
+    df = pd.read_csv(file_path, header=None, sep=' ')
+    dat_bins = df[0].values
+    file_ind = np.where(dat_bins == file)[0][0]
+    dat_times = np.asarray([line.split('+/-')[0] for line in df[ind].values], dtype=np.float)
+    dat_errors = np.asarray([line.split('+/-')[1] for line in df[ind].values], dtype=np.float)
+    return dat_times[file_ind], dat_errors[file_ind]
+
 def shift_Max(seis, pred_var):
     data = seis.data
     time = seis.times()
@@ -63,7 +76,6 @@ def shift_Max(seis, pred_var):
     return arrival    
 
 cap = '5'
-file = 'n35_128.10'
 
 pred_file = pd.read_csv('cross_secs_dat/model_pred/'+cap+'caps_deg_results.csv')
 
@@ -73,67 +85,64 @@ pred_file = pd.read_csv('cross_secs_dat/model_pred/'+cap+'caps_deg_results.csv')
 qual_similarity = pred_file['410qual'].values / pred_file['660qual'].values
 similar_inds = np.where((qual_similarity > 0.9) & (qual_similarity < 1.1))
 similar_sort = np.argsort(qual_similarity[similar_inds])
-#quality_sort = np.argsort(pred_file['410qual'].values[similar_inds][qs_sort])
+qualities = np.round(pred_file[['410qual', '660qual']].values[similar_inds][similar_sort],2)
+files = pred_file['file'].values[similar_inds][similar_sort]
+arrivals410 = pred_file['410pred'].values[similar_inds][similar_sort]
+arrivals660 = pred_file['660pred'].values[similar_inds][similar_sort]
 
 qual_dic = {'0.1': 173, '0.2': 219, '0.3': 81,'0.4': 233, '0.5': 264,
             '0.6': 203,'0.7': 224,'0.8': 191,'0.9': 209,'1.0': 296}
 
-
-
-
-
-
-'''
-cs = obspy.read('../../seismograms/cross_secs/' + cap + 'caps_deg/' + file + '.sac')
-
-with open('cross_secs_dat/model_pred/'+cap+'caps_deg_results.csv') as pred_csv:
-    for line in pred_csv:
-        if file == line.split(',')[0].rstrip('.sac'):
-            pred_line = line
-            break
-
-cs = cs[0].resample(10)
-
-shift = -cs.stats.sac.b
-times = cs.times() - shift
-b = cs.stats.sac.b + shift
-e = cs.stats.sac.e + shift
-arr_410 = float(pred_line.split(',')[1])
-arr_660 = float(pred_line.split(',')[5])
-qual_410 = float(pred_line.split(',')[4])
-qual_660 = float(pred_line.split(',')[8])
-quals = [qual_660, qual_410]
-try:
-    lauren_410 = get_Lauren_Pred(cap, '410', file)# - 4.2 # temp 4.2 shift from error by lauren
-    lauren_660 = get_Lauren_Pred(cap, '660', file)#ls - 4.2
-except:
-    pass
-
-cs_norm = cs.data / np.abs(cs.data).max()
-fig, ax = plt.subplots()
-ax.plot(times, cs_norm, color='black')
-for i, ar in enumerate([arr_660, arr_410]):
-    print(ar)
-    ax.axvline(ar, color='blue', linestyle='--')
-    ax.text(ar-15, 0.1, quals[i], rotation=90, fontsize=16)
-ax.axvline(ar, color='blue', linestyle='--', label='model')
-try:
-    for i, ar in enumerate([lauren_660, lauren_410]):
-        ax.axvline(ar, color='red', linestyle=':')
-    ax.axvline(ar, color='red', linestyle=':', label='lauren')
-except:
-    pass
-#for i, ar in enumerate(arrivals_neg[np.argsort(counts_neg)][-5:]):
-#   ax.axvline(ar, color='red', linestyle='--')
-#   ax.text(ar-5, 0.1, np.sort(counts_neg)[-5:][i], rotation=90, fontsize=16)
-ax.axvline(ar, color='red', linestyle='--', label='negative model')
-ax.set_ylim(cs_norm.min(), cs_norm.max())
-ax.set_xlim(times.min(), times.max())
-ax.set_xlabel('Time [s]')
-ax.set_ylabel('Amplitude')
-ax.set_title(cap + 'cap/' + file)
-ax.xaxis.set_minor_locator(mtick.MultipleLocator(10))
-#ax.legend(loc='upper right')
-fig.tight_layout()
-plt.show()
-'''
+for i, key in enumerate(qual_dic):
+    index = qual_dic[key]
+    file = files[index]
+    arr_410 = arrivals410[index]
+    arr_660 = arrivals660[index]
+    qual_410 = qualities[index,0]
+    qual_660 = qualities[index,1]
+    quals = [qual_660, qual_410]
+    
+    try:
+        lauren_410 = get_Lauren_Pred(cap, '410', file)
+        lauren_660 = get_Lauren_Pred(cap, '660', file)
+        lerr_410 = get_Lauren_Pred(cap, '410', file)
+        lerr_660 = get_Lauren_Pred(cap, '660', file)
+        l_errors = [lerr_660, lerr_410]
+    except:
+        pass
+    
+    cs = obspy.read('../../seismograms/cross_secs/' + cap + 'caps_deg/' + file + '.sac')
+    cs = cs[0].resample(10)
+    
+    shift = -cs.stats.sac.b
+    times = cs.times() - shift
+    b = cs.stats.sac.b + shift
+    e = cs.stats.sac.e + shift
+    
+    cs_norm = cs.data / np.abs(cs.data).max()
+    fig, ax = plt.subplots()
+    ax.plot(times, cs_norm, color='black')
+    for i, ar in enumerate([arr_660, arr_410]):
+        print(ar)
+        ax.axvline(ar, color='blue', linestyle='--')
+        ax.text(ar-15, 0.2, quals[i], rotation=90, fontsize=16)
+    ax.axvline(ar, color='blue', linestyle='--', label='model')
+    try:
+        for i, ar in enumerate([lauren_660, lauren_410]):
+            ax.axvline(ar, color='red', linestyle=':')
+        ax.axvline(ar, color='red', linestyle=':', label='lauren')
+    except:
+        pass
+    #for i, ar in enumerate(arrivals_neg[np.argsort(counts_neg)][-5:]):
+    #   ax.axvline(ar, color='red', linestyle='--')
+    #   ax.text(ar-5, 0.1, np.sort(counts_neg)[-5:][i], rotation=90, fontsize=16)
+    #   ax.axvline(ar, color='red', linestyle='--', label='negative model')
+    ax.set_ylim(cs_norm.min(), cs_norm.max())
+    ax.set_xlim(times.min(), times.max())
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Amplitude')
+    ax.set_title(cap + 'cap/' + file)
+    ax.xaxis.set_minor_locator(mtick.MultipleLocator(10))
+    ax.legend(loc='upper left')
+    fig.tight_layout(pad=0.5)
+    plt.show()
