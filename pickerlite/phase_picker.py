7d53df753cab833202bc4f47f19d16d811efbb9a
Jorge Garcia <jorgeagr@pop-os.localdomain>
2020-03-03 01:43:15 -0700

    Update

--- pickerlite/phase_picker.py
+++ pickerlite/phase_picker.py
@@ -21,11 +21,14 @@ parser = argparse.ArgumentParser(description='Predict precursor arrivals in vesp
 parser.add_argument('file_dir', help='Cross-section SAC files directory.', type=str)
 parser.add_argument('phase', help='Phase to pick for.', type=str)
 parser.add_argument('model_name', help='Path to model H5 file.', type=str)
+parser.add_argument('-o', '--overwrite', help='Whether to the picker should overwrite the original SAC file. Default is true.',
+                    type=bool, default=True)
 args = parser.parse_args()
 
 file_dir = args.file_dir
 model_name = args.model_name
 phase = args.phase
+overwrite = args.overwrite
 
 if file_dir[-1] != '/':
     file_dir += '/'
@@ -139,12 +142,17 @@ def pick_Phase(file_dir, seis_file, phase_name, model, store_header='auto', rele
     seis.stats.sac['k'+phase_var] = phase_name+'ap'
     seis.stats.sac['user'+phase_var[-1]] = np.round(arrival_qual*100)
     seis.stats.sac['kuser0'] = 'PickQual'
-    seis.write(file_dir + 'picked/' + seis_file.rstrip('.s_fil') + '_auto' + '.sac')
+    if overwrite:
+        seis.write(file_dir + seis_file.rstrip('.s_fil') + '.sac')
+        os.replace(file_dir + seis_file.rstrip('.s_fil') + '.sac',
+                   file_dir + seis_file)
+    else:
+        seis.write(file_dir + 'picked/'+ seis_file.rstrip('.s_fil') + '_auto' + '.sac')
     
     return
 
 def write_Exception(file_dir, file_num, seis_file, exception, mode='a'):
-    with open(file_dir + 'picked/bad.log', mode) as log:
+    with open(file_dir + 'picked/'*~overwrite + 'bad.log', mode) as log:
         print('File {}: {}'.format(f+1, seis_file), file=log)
         print('Error: {}'.format(exception), end='\n\n', file=log)
 
@@ -160,8 +168,9 @@ model = tf.keras.models.load_model(model_path+model_name)
 files = np.sort([f for f in os.listdir(file_dir) if '.s_fil' in f])
 gen_whitespace = lambda x: ' '*len(x)
 
-if 'picked' not in os.listdir(file_dir):
-    os.mkdir(file_dir + 'picked/')
+if not overwrite:
+    if 'picked' not in os.listdir(file_dir):
+        os.mkdir(file_dir + 'picked/')
 
 print('\nPicking for', phase, 'phase in', len(files), 'files.')
 for f, seis_file in enumerate(files):
@@ -172,7 +181,7 @@ for f, seis_file in enumerate(files):
     except (KeyboardInterrupt, SystemExit):
         raise
     except Exception as exception:
-        if 'bad.log' not in os.listdir(file_dir + 'picked/'):
+        if 'bad.log' not in os.listdir(file_dir + 'picked/'*~overwrite):
             write_Exception(file_dir, f, seis_file, exception, mode='w+')
         else:
             write_Exception(file_dir, f, seis_file, exception)
