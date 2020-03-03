b16ba20bcdfaeb297684df997ea234cd1fc01517
Jorge A. Garcia <jorgeagr97@gmail.com>
2020-03-03 15:20:04 -0800

    Update

--- pickerlite/split_files.py
+++ pickerlite/split_files.py
@@ -17,19 +17,21 @@ args = parser.parse_args()
 
 file_dir = args.file_dir
 partitions = args.partitions
+if file_dir[-1] != '/':
+    file_dir += '/'
 
 files = list(map(lambda x: file_dir+x, sorted(os.listdir(file_dir))))
 
 part_size = len(files) // partitions
-#remainder = len(files) % partitions
 
 for i in range(partitions):
-    os.mkdir(file_dir + '{}/'.format(i))
+    partdir = '{}{}'.format(file_dir.split('/')[-2], i)
+    os.mkdir(file_dir + partdir)#'{}{}/'.format(file_dir.split('/')[-2],i))
     if i != partitions-1:
         part_files = files[part_size*i:part_size*(i+1)]
     else:
         part_files = files[part_size*i:]
     part_files.insert(0, 'mv')
     part_files.append('-t')
-    part_files.append(file_dir + '{}/'.format(i))
+    part_files.append(file_dir + partdir)#'{}{}/'.format(i))
     subprocess.call(part_files)
